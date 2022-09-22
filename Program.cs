871c98d40d6cf328e07b372bd95367bc4f2ba9a3
FlpSlva <flpslvadev@gmail.com>
2022-09-22 07:19:37 -0300

    create connectionString

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace ConnectionDapper
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+             SqlConnection con = new SqlConnection();
+             con.ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+             
+            using(var connection = new SqlConnection(con.ConnectionString))
+            {
+               
+            }
+
+        }
+    }
+}
