1720699a886aedd6550c472199b4be9a55a06a60
FlpSlva <flpslvadev@gmail.com>
2022-09-24 17:14:39 -0300

    criaçao novos metodos

--- Program.cs
+++ Program.cs
@@ -13,9 +13,10 @@ namespace ConnectionDapper
 
             using(var connection = new SqlConnection(con.ConnectionString))
             {   
-             // ListCategories(connection);
+              ListCategories(connection);
+              CreateManyCategory(connection);
              // CreateCategory(connection);
-              UpdateCategory(connection);
+             // UpdateCategory(connection);
             }
 
         }
@@ -77,6 +78,72 @@ namespace ConnectionDapper
             });
             Console.WriteLine("Campo Atualizado Com Sucesso!");
 
+        }
+
+
+        static void CreateManyCategory(SqlConnection connection)
+        {
+
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Categoria Nova";
+            category2.Url = "categoria-nova";
+            category2.Description = "Categoria Nova";
+            category2.Order = 9;
+            category2.Summary = "Categoria";
+            category2.Featured = true;
+
+
+            var insertSql = @"INSERT INTO
+            [Category]
+             VALUES(
+                @ID,
+                @Title,
+                @Url,
+                @Summary,
+                @Order,
+                @Description,
+                @Featured)";
+
+            var rows = connection.Execute(insertSql, new[]{
+                new{
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured  
+                },
+                new
+                {
+                    category2.Id,
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                }
+                    
+                   
+            });
+               Console.WriteLine($"{rows} linhas inseridas");
+            
+        }
+
+        static void ExecuteProcedure(SqlConnection connection)
+        {
+            
         }
     }
 }
